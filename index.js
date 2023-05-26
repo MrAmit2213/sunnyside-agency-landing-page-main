2b0d23185266d6b0a631c8cb746461645d5e083e
MrAmit2213 <amitkumar94800@gmail.com>
2023-05-27 00:56:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function show(){
+    if(document.getElementById('menu').style.display!='none'){
+        document.getElementById("menu").style.display='none';
+    }
+    else{
+        document.getElementById("menu").style.display='flex';
+    }
+}
